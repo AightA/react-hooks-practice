2dc383862bd6a4a9d2778f7cab80a4e6a7421379
Ahmed Merehil <ahmed8merehil@yahoo.com>
2021-07-10 18:46:35 +0100

    inital start with react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [space, setSpace] = useState([]);
+  const [arr, setArr] = useState([1, 2, 3, 4, 5, 6, 7]);
+
+  // const API_URL = `https://api.spacexdata.com/v3/launches`;
+
+  useEffect(() => {
+    // fetch the API JSON.data
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    // const res = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
+    const res = await fetch(`https://api.spacexdata.com/v3/launches`);
+
+    const data = await res.json();
+    setSpace(data);
+  };
+  console.log('space', space.splice(0, 1));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Welcome to first fetch API project</h1>
       </header>
+      <div>
+        <h1>Flight list </h1>
+        {space
+          .map((item, index) => {
+            return (
+              <div>
+                <ul key={index}>
+                  <li>Flight number: {item.flight_number}</li>
+                  <br />
+                  <li>Mission name: {item.mission_name}</li>
+                  <br />
+                  <li>Launch Year: {item.launch_year}</li>
+                  <br />
+                  {
+                    <img
+                      src={item.links.mission_patch}
+                      alt='space patch'
+                      style={{
+                        height: '100px',
+                        width: '100px',
+                        border: '3px blue solid',
+                        borderRadius: '15px',
+                      }}
+                    />
+                  }
+                </ul>
+              </div>
+            );
+          })
+          .splice(0, 10)}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
